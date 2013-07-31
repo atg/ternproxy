278dc73587a201cf903783d7a3c60a29e0b26f37
Sérgio Ramos <mail@sergioramos.me>
2013-07-31 21:18:57 +0100

    parse fields.heuristics

--- src/router.js
+++ src/router.js
@@ -24,19 +24,20 @@ module.exports = function () {
       var form = new formidable.IncomingForm()
       var method = req.method.toLowerCase()
       form.encoding = 'utf-8'
-
+      
       if(!routes[method][req.url]) return utils.http.respond(req, res)(null, null, 404)
-
+      
       form.parse(req, function(e, fields, files) {
         if(e) throw e
-        
+      
         if(fields.cursor_position) fields.cursor_position = Number(fields.cursor_position)
         if(fields.delta_length) fields.delta_length = Number(fields.delta_length)
         if(fields.delta_offset) fields.delta_offset = Number(fields.delta_offset)
         if(fields.sending_full_content) fields.sending_full_content = JSON.parse(fields.sending_full_content)
-        
+        if(fields.heuristics) fields.heuristics = JSON.parse(fields.heuristics)
+      
         req.body = fields
-        
+      
         routes[method][req.url](req, res)
       })
     }
