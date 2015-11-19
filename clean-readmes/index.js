0f116e00303ba248010778b65d7029bc2618f90b
Sérgio Ramos <mail@sergioramos.me>
2015-11-19 23:39:31 +0000

    also remove ignored

--- clean-readmes/index.js
+++ clean-readmes/index.js
@@ -2,28 +2,47 @@ var glob = require('glob')
 var intoStream = require('into-stream')
 var through = require('through2')
 var path = require('path')
+var rimraf = require('rimraf')
 var fs = require('fs')
 
+var onIgnored = function(file) {
+  var filename = path.join(__dirname, '..', file)
+  rimraf.sync(filename)
+}
 
-glob('**/*/package.json', function(err, files) {
-  if (err) {
-    throw err
+var onIgnore = function(ignore) {
+  if (!ignore) {
+    return
   }
 
-  var onFile = through.obj(function(chunk, enc, fn) {
-    var filename = path.join(process.cwd(), chunk.toString())
-    var pkg = require(filename)
+  glob.sync(ignore).forEach(onIgnored)
+}
 
-    Object.keys(pkg).forEach(function(key) {
-      if (!(/^\_|^readme|^readmeFilename/).test(key)) {
-        return
-      }
+var cleanIgnores = function() {
+  var filename = path.join(__dirname, '..', '.gitignore')
+  var ignores = fs.readFileSync(filename, 'utf-8').split(/\n/)
+  ignores.forEach(onIgnore)
+}
 
-      pkg[key] = undefined
-    })
+var onPkg = function(file) {
+  var filename = path.join(__dirname, '..', file)
 
-    fs.writeFile(filename, JSON.stringify(pkg, null, 2), 'utf-8', fn)
+  if (!fs.existsSync(filename)) {
+    return
+  }
+
+  var pkg = require(filename)
+  Object.keys(pkg).forEach(function(key) {
+    var regex = /^\_|^readme|^readmeFilename/
+    pkg[key] = !regex.test(key) ? pkg[key] : undefined
   })
 
-  intoStream(files).pipe(onFile).pipe(process.stdout)
-})
+  fs.writeFileSync(filename, JSON.stringify(pkg, null, 2), 'utf-8')
+}
+
+var cleanReadmes = function() {
+  glob.sync('**/*/package.json').forEach(onPkg)
+}
+
+cleanReadmes()
+cleanIgnores()
