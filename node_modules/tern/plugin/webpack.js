7e8706e1d02b6d11cf5eeec95f4292aab256e7ba
Sérgio Ramos <mail@sergioramos.me>
2017-01-27 13:28:33 +0000

    update dependencies

--- node_modules/tern/plugin/webpack.js
+++ node_modules/tern/plugin/webpack.js
@@ -11,22 +11,28 @@ var path = require("path");
 var ResolverFactory = require("enhanced-resolve").ResolverFactory;
 var SyncNodeJsInputFileSystem = require("enhanced-resolve/lib/SyncNodeJsInputFileSystem");
 
-function getResolver(configPath) {
+function getResolver(modules, configPath) {
   var config = {
     unsafeCache: true,
-    modules: ["node_modules"],
+    modules: modules || ["node_modules"],
     extensions: [".js", ".jsx", ".json"],
     aliasFields: ["browser"],
     mainFields: ["browser", "web", "browserify", "main"],
     fileSystem: new SyncNodeJsInputFileSystem()
   }
-  var resolveConfig = fs.existsSync(configPath) ? require(configPath).resolve : null
+  var webpackConfig = (configPath && fs.existsSync(configPath)) ? require(configPath) : null
+  if (typeof webpackConfig === 'function') {
+    webpackConfig = webpackConfig()
+  }
+  var resolveConfig = webpackConfig && webpackConfig.resolve
   if (resolveConfig) {
     Object.keys(resolveConfig).forEach(function (key) {
       if (key === 'packageMains') {
         config.mainFields = resolveConfig[key]
       } else if (key === 'root') {
         config.modules.unshift(resolveConfig[key])
+      } else if (key === 'modules') {
+        config.modules = config.modules.concat(resolveConfig[key])
       } else {
         config[key] = resolveConfig[key]
       }
@@ -49,8 +55,9 @@ function resolveToFile(resolver, name, parentFile) {
 
 tern.registerPlugin("webpack", function(server, options) {
   var configPath = options.configPath || './webpack.config.js'
+  var modules = options.modules || ['node_modules']
   configPath = path.resolve(server.options.projectDir, configPath)
-  var resolver = getResolver(configPath)
+  var resolver = getResolver(modules, configPath)
   server.loadPlugin("commonjs")
   server.loadPlugin("es_modules")
   server.mod.modules.resolvers.push(function (name, parentFile) {
